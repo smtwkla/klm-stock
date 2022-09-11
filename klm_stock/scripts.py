6075f9d9f957b28546fbeaebd26bd494112c11b6
KLA <smtwkla@gmail.com>
2022-09-11 23:00:07 +0530

    scripts.py with func declarations to be implemented

---
+++ klm_stock/scripts.py
@@ -0,0 +1,16 @@
+
+
+def list_scripts():
+    print("List of scripts...")
+
+
+def add_script():
+    print("Enter script details...")
+
+
+def edit_script():
+    print("Enter script code to edit:")
+
+
+def del_script():
+    print("Enter script code to delete:")
