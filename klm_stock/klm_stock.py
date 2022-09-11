4e9e72b9f9e0eca2fe131898efd8c9b6992eaf51
KLA <smtwkla@gmail.com>
2022-09-11 22:57:21 +0530

    while loop to execute menu cmds

--- klm_stock/klm_stock.py
+++ klm_stock/klm_stock.py
@@ -1,5 +1,22 @@
 import klm_menu
 import secrets
 import menu_def
+import scripts
 
-klm_menu.present_menu(menu_def.main_menu, menu_def.menu_system)
+menu = menu_def.main_menu
+
+while menu:
+    cmd, menu_name = klm_menu.present_menu(menu, menu_def.menu_system)
+    menu = menu_def.menu_system[menu_name]
+    if cmd == "exit":
+        menu = None
+        continue
+    # execute cmd
+    if cmd == "list_scripts":
+        scripts.list_scripts()
+    elif cmd == "add_script":
+        scripts.add_script()
+    elif cmd == "edit_script":
+        scripts.edit_script()
+    elif cmd == "del_script":
+        scripts.del_script()
