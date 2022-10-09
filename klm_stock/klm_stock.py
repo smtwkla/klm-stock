36747e4e7b7ad1c6554f13a25f79cf5f529e48f8
KLA <smtwkla@gmail.com>
2022-10-09 19:05:29 +0530

    working ledger

--- klm_stock/klm_stock.py
+++ klm_stock/klm_stock.py
@@ -4,6 +4,7 @@ import klm_menu
 import db_functions
 import scripts
 import sectors
+import transactions
 
 import app_secrets
 import menu_def
@@ -29,3 +30,9 @@ while menu_name:
         scripts.del_script()
     elif cmd == "list_sectors":
         sectors.list_sectors()
+    elif cmd == "buy":
+        transactions.add_trasaction(is_buy=True)
+    elif cmd == "sell":
+        transactions.add_trasaction(is_buy=False)
+    elif cmd == "ledger":
+        transactions.print_script_ledger()
---
+++ klm_stock/portfolio.py
@@ -0,0 +1,14 @@
+def get_portfolio():
+    pass
+
+
+def get_db_LTP():
+    pass
+
+
+def fetch_LTP():
+    pass
+
+
+def get_purchase_details():
+    pass
